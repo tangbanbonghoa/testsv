fbb4c928c9b65a2b4fe2bde7136594736f40cdb5
tangbanbonghoa <151010788+tangbanbonghoa@users.noreply.github.com>
2023-11-25 15:23:57 +0700

    Add files via upload

---
+++ B_BUS/Services/Services.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A_DAL.Respositories;
+using A_DAL.IRespositories;
+using A_DAL.Models;
+
+namespace B_BUS.Services
+{
+    public class Services
+    {
+        static IRespository respository;
+        static Services()
+        {
+            respository = new Respository();
+        }
+        public static List<Cuahang> GetAll()
+        {
+            return respository.GetAll();
+        }
+        public static Cuahang GetByID(int id)
+        {
+            return respository.GetByID(id);
+        }
+        public static Cuahang Insert(Cuahang obj)
+        {
+            return respository.Insert(obj);
+        }
+        public void Update(Cuahang obj)
+        {
+            respository.Update(obj);
+        }
+        public static void Delete(Cuahang obj)
+        {
+            respository.Delete(obj);
+        }
+    }
+}
